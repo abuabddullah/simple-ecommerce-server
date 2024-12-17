e41bce23b04ec27eaa6816de378f783e302616aa
asifaowadud <asifaowadud@gmail.com>
2024-12-18 03:10:06 +0600

    sslcommerz partially done

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -1,7 +1,216 @@
-const paypal = require("../../helpers/paypal");
+// const paypal = require("../../helpers/paypal");
+// const Order = require("../../models/Order");
+// const Cart = require("../../models/Cart");
+// const Product = require("../../models/Product");
+// require("dotenv").config();
+
+// const createOrder = async (req, res) => {
+//   try {
+//     const {
+//       userId,
+//       cartItems,
+//       addressInfo,
+//       orderStatus,
+//       paymentMethod,
+//       paymentStatus,
+//       totalAmount,
+//       orderDate,
+//       orderUpdateDate,
+//       paymentId,
+//       payerId,
+//       cartId,
+//     } = req.body;
+
+//     const create_payment_json = {
+//       intent: "sale",
+//       payer: {
+//         payment_method: "paypal",
+//       },
+//       redirect_urls: {
+//         return_url: `${process.env.FRONTEND_URL}/shop/paypal-return`,
+//         cancel_url: `${process.env.FRONTEND_URL}/shop/paypal-cancel`,
+//       },
+//       transactions: [
+//         {
+//           item_list: {
+//             items: cartItems.map((item) => ({
+//               name: item.title,
+//               sku: item.productId,
+//               price: item.price.toFixed(2),
+//               currency: "USD",
+//               quantity: item.quantity,
+//             })),
+//           },
+//           amount: {
+//             currency: "USD",
+//             total: totalAmount.toFixed(2),
+//           },
+//           description: "description",
+//         },
+//       ],
+//     };
+
+//     paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
+//       if (error) {
+//         console.log(error);
+
+//         return res.status(500).json({
+//           success: false,
+//           message: "Error while creating paypal payment",
+//         });
+//       } else {
+//         const newlyCreatedOrder = new Order({
+//           userId,
+//           cartId,
+//           cartItems,
+//           addressInfo,
+//           orderStatus,
+//           paymentMethod,
+//           paymentStatus,
+//           totalAmount,
+//           orderDate,
+//           orderUpdateDate,
+//           paymentId,
+//           payerId,
+//         });
+
+//         await newlyCreatedOrder.save();
+
+//         const approvalURL = paymentInfo.links.find(
+//           (link) => link.rel === "approval_url"
+//         ).href;
+
+//         res.status(201).json({
+//           success: true,
+//           approvalURL,
+//           orderId: newlyCreatedOrder._id,
+//         });
+//       }
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
+
+// const capturePayment = async (req, res) => {
+//   try {
+//     const { paymentId, payerId, orderId } = req.body;
+
+//     let order = await Order.findById(orderId);
+
+//     if (!order) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Order can not be found",
+//       });
+//     }
+
+//     order.paymentStatus = "paid";
+//     order.orderStatus = "confirmed";
+//     order.paymentId = paymentId;
+//     order.payerId = payerId;
+
+//     for (let item of order.cartItems) {
+//       let product = await Product.findById(item.productId);
+
+//       if (!product) {
+//         return res.status(404).json({
+//           success: false,
+//           message: `Not enough stock for this product ${product.title}`,
+//         });
+//       }
+
+//       product.totalStock -= item.quantity;
+
+//       await product.save();
+//     }
+
+//     const getCartId = order.cartId;
+//     await Cart.findByIdAndDelete(getCartId);
+
+//     await order.save();
+
+//     res.status(200).json({
+//       success: true,
+//       message: "Order confirmed",
+//       data: order,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
+
+// const getAllOrdersByUser = async (req, res) => {
+//   try {
+//     const { userId } = req.params;
+
+//     const orders = await Order.find({ userId });
+
+//     if (!orders.length) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "No orders found!",
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       data: orders,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
+
+// const getOrderDetails = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+
+//     const order = await Order.findById(id);
+
+//     if (!order) {
+//       return res.status(404).json({
+//         success: false,
+//         message: "Order not found!",
+//       });
+//     }
+
+//     res.status(200).json({
+//       success: true,
+//       data: order,
+//     });
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).json({
+//       success: false,
+//       message: "Some error occured!",
+//     });
+//   }
+// };
+
+// module.exports = {
+//   createOrder,
+//   capturePayment,
+//   getAllOrdersByUser,
+//   getOrderDetails,
+// };
+
 const Order = require("../../models/Order");
 const Cart = require("../../models/Cart");
 const Product = require("../../models/Product");
+const axios = require("axios"); // Importing Axios
 require("dotenv").config();
 
 const createOrder = async (req, res) => {
@@ -21,72 +230,83 @@ const createOrder = async (req, res) => {
       cartId,
     } = req.body;
 
-    const create_payment_json = {
-      intent: "sale",
-      payer: {
-        payment_method: "paypal",
-      },
-      redirect_urls: {
-        return_url: `${process.env.FRONTEND_URL}/shop/paypal-return`,
-        cancel_url: `${process.env.FRONTEND_URL}/shop/paypal-cancel`,
-      },
-      transactions: [
-        {
-          item_list: {
-            items: cartItems.map((item) => ({
-              name: item.title,
-              sku: item.productId,
-              price: item.price.toFixed(2),
-              currency: "USD",
-              quantity: item.quantity,
-            })),
-          },
-          amount: {
-            currency: "USD",
-            total: totalAmount.toFixed(2),
-          },
-          description: "description",
-        },
-      ],
+    // 1- initiate Payment with default sandbox account
+    const paymentInitiatingData = {
+      store_id: process.env.SSLCOMMERZ_STORE_ID,
+      store_passwd: process.env.SSLCOMMERZ_STORE_PASSWORD,
+      total_amount: totalAmount,
+      currency: "BDT",
+      tran_id: "REF123",
+      success_url: `${process.env.FRONTEND_URL}/shop/paypal-return`,
+      fail_url: `${process.env.FRONTEND_URL}/shop/unauth-page`,
+      cancel_url: `${process.env.FRONTEND_URL}/shop/paypal-cancel`,
+      cus_name: userId,
+      cus_email: userId,
+      cus_add1: addressInfo?.address,
+      cus_add2: "Bangladesh",
+      cus_city: addressInfo?.city,
+      cus_state: "Bangladesh",
+      cus_postcode: addressInfo?.pincode,
+      cus_country: "Bangladesh",
+      cus_phone: addressInfo?.phone,
+      cus_fax: addressInfo?.phone,
+      shipping_method: "NO",
+      product_name: "Anonymous",
+      product_category: "Anonymous",
+      product_profile: "general",
+      multi_card_name: "mastercard,visacard,amexcard",
+      value_a: "ref001_A",
+      value_b: "ref002_B",
+      value_c: "ref003_C",
+      value_d: "ref004_",
     };
-
-    paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
-      if (error) {
-        console.log(error);
-
-        return res.status(500).json({
+    //2. Do post req to sslcommerz server by https://sandbox.sslcommerz.com/gwprocess/v4/api.php
+    const sslServerResponse = await axios({
+      method: "POST",
+      url: "https://sandbox.sslcommerz.com/gwprocess/v4/api.php",
+      data: paymentInitiatingData,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    })
+      .then((res) => {
+        console.log("🚀 ~ createOrder ~ res:", res?.data?.GatewayPageURL);
+        return res;
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        res.status(500).json({
           success: false,
-          message: "Error while creating paypal payment",
+          message: "Some error occured!",
         });
-      } else {
-        const newlyCreatedOrder = new Order({
-          userId,
-          cartId,
-          cartItems,
-          addressInfo,
-          orderStatus,
-          paymentMethod,
-          paymentStatus,
-          totalAmount,
-          orderDate,
-          orderUpdateDate,
-          paymentId,
-          payerId,
-        });
+      });
 
-        await newlyCreatedOrder.save();
+    //3. add order to database and response to client
 
-        const approvalURL = paymentInfo.links.find(
-          (link) => link.rel === "approval_url"
-        ).href;
-
-        res.status(201).json({
-          success: true,
-          approvalURL,
-          orderId: newlyCreatedOrder._id,
-        });
-      }
+    const newlyCreatedOrder = new Order({
+      userId,
+      cartId,
+      cartItems,
+      addressInfo,
+      orderStatus,
+      paymentMethod,
+      paymentStatus,
+      totalAmount,
+      orderDate,
+      orderUpdateDate,
+      paymentId,
+      payerId,
     });
+
+    await newlyCreatedOrder.save();
+
+    res.status(201).json({
+      success: true,
+      approvalURL: sslServerResponse?.data?.GatewayPageURL,
+      orderId: newlyCreatedOrder._id,
+    });
+
+    
   } catch (e) {
     console.log(e);
     res.status(500).json({
@@ -97,6 +317,11 @@ const createOrder = async (req, res) => {
 };
 
 const capturePayment = async (req, res) => {
+  const paymentSuccessData = req.body();
+  console.log(
+    "🚀 ~ payment captured and hitted success url",
+    paymentSuccessData
+  );
   try {
     const { paymentId, payerId, orderId } = req.body;
 
@@ -154,7 +379,7 @@ const getAllOrdersByUser = async (req, res) => {
 
     const orders = await Order.find({ userId });
 
-    if (!orders?.length) {
+    if (!orders.length) {
       return res.status(404).json({
         success: false,
         message: "No orders found!",
